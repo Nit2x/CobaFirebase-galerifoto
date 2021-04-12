1d2fd5ec7ec8685fc29c9e815fb0e74e4469138d
Nit2x <anitaforpetra@gmail.com>
2021-04-13 00:06:43 +0700

    DONE individual

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
+import { fileFoto } from './../tab4/tab4.page';
+import { FotoService, Photo } from './../services/foto.service';
 import { Component } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +11,42 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    public fotoService:FotoService,
+    public afStorage : AngularFireStorage,
+    public toastCtrl : ToastController
+    ) {}
+
+  async ngOnInit() {
+    await this.fotoService.loadFoto();
+  }
+
+  TambahFoto() {
+    this.fotoService.tambahFoto();
+  }
+
+  fototitle : string;
+  fotoUpload : Photo;
+
+  getNama(judulFoto : Photo) {
+    this.fotoUpload = judulFoto;
+    this.fototitle = judulFoto.filePath;
+  }
+
+  uploadData() {
+    const imgfilepath = `filestorage/${this.fotoUpload.filePath}`;
+    this.afStorage.upload(imgfilepath, this.fotoUpload.dataImage).then(()=> {
+      console.log(this.fotoUpload);
+      this.showToast();
+    });
+  }
+
+  async showToast() {
+    await this.toastCtrl.create({
+      message: 'Upload Selesai',
+      duration : 2000,
+      position : 'middle'
+    }).then(res => res.present());
+  }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAVz8kXi6azUCqsUIcbOZ0dttNm-nSIsAE",
+    authDomain: "projectionic-nit2x.firebaseapp.com",
+    projectId: "projectionic-nit2x",
+    storageBucket: "projectionic-nit2x.appspot.com",
+    messagingSenderId: "635367580893",
+    appId: "1:635367580893:web:9709b00e8af4d1620ca734",
+    measurementId: "G-WQD3X6LBGK"
+  }
 };
 
 /*
